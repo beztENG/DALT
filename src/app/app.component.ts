e34a023393d4a8eee636e63a080d1875fd1f993c
Dinh Quoc Vinh <binkkboi@gmail.com>
2024-03-13 19:39:30 +0700

    Update phan cua Vinh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'english-course-website';
+
+  images = [
+    {
+      imgSrc: 'assets/image1.jpg',
+      imgAlt: 'image 1',
+    },
+    {
+      imgSrc: 'assets/image2.jpg',
+      imgAlt: 'image 2',
+    },
+    {
+      imgSrc: 'assets/image3.jpg',
+      imgAlt: 'image 3',
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideshowComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HammerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
