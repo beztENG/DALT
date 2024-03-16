c831ae047c0a9efdc7178e1b02ecb367bd13df53
ThoxNe <thantc234@gmail.com>
2024-03-16 20:50:53 +0700

    them component bottom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BottomComponent } from './bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule,
