d31779576314aff3cbdca9837c873159dbacaaef
ThoxNe <thantc234@gmail.com>
2024-04-10 14:29:10 +0700

    khoi tap api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,8 +8,36 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:4200"]
+    origin: ["http://localhost:4233"]
 }));
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://thandh23044:Than23032003@englishwebproject.h2gd5cq.mongodb.net/?retryWrites=true&w=majority&appName=EnglishWebProject";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.use("/api/events",eventsRouter)
